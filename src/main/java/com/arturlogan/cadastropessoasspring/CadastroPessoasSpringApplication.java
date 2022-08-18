8851bafefc512e8a908318ddf7473b82773a5eff
Artur Logan <arturlogan1ferreira@gmail.com>
2022-08-18 15:56:25 -0300

    Commit Inicial.

---
+++ src/main/java/com/arturlogan/cadastropessoasspring/CadastroPessoasSpringApplication.java
@@ -0,0 +1,13 @@
+package com.arturlogan.cadastropessoasspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroPessoasSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroPessoasSpringApplication.class, args);
+	}
+
+}
